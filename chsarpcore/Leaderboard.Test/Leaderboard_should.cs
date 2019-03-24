3efa1253019774ac1723fbc653889e965fadfe90
Sébastien Canonica <sebcano.biz@laposte.net>
2019-03-24 21:57:55 +0100

    Create and use fake events source

--- chsarpcore/Leaderboard.Test/Leaderboard_should.cs
+++ chsarpcore/Leaderboard.Test/Leaderboard_should.cs
@@ -1,4 +1,5 @@
 ﻿using Abbotware.Interop.NUnit;
+using FluentAssertions.Json;
 using LeaderboardApp;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
@@ -25,18 +26,47 @@ namespace Leaderboard.Test
             var server = new TestServer(
                 new WebHostBuilder().UseStartup<Startup>()                    
             );
-            var client = server.CreateClient();
 
-            var result = await client.GetAsync("/leaderboard");
-            
-            result.EnsureSuccessStatusCode();
-            var resultString = await result.Content.ReadAsStringAsync();
-            var resultLeaderboard = JArray.Parse(resultString);
-            dynamic firstGame = resultLeaderboard.First;
+            var actual = await FetchLeaderboard(server);
+
+            dynamic firstGame = actual.First;
             Check.ThatDynamic(firstGame.home.Value).IsEqualTo("Lyon");
             Check.ThatDynamic(firstGame.visitor.Value).IsEqualTo("Marseille");
             Check.ThatDynamic(firstGame.state.Value).IsEqualTo("in progress");
             Check.ThatDynamic(firstGame.score.Count).IsEqualTo(2);
+        }
+
+        [Test]
+        public async Task Display_a_leaderboard_with_1_game_started()
+        {
+            var server = CreateServer(
+                new FootballEvent { type = "game-start", gameId = "uriage-meylan" }
+            );
+
+            var actual = await FetchLeaderboard(server);
+
+            actual.Should().BeEquivalentTo(JToken.Parse(@"[
+                { ""home"": ""Uriage"", ""visitor"": ""Meylan"", ""state"": ""in progress"", ""score"": [0, 0] }
+            ]"));
+        }
+
+        private TestServer CreateServer(params FootballEvent[] fakeEvents)
+        {
+            var fakeSource = new FakeFootballEventSource(fakeEvents);
+            return new TestServer(
+                new WebHostBuilder()
+                    .UseStartup<Startup>()
+                    .ConfigureTestServices(services => services.AddSingleton<IFootballEventsSource>(fakeSource))
+            );
+        }
+
+        private async Task<JArray> FetchLeaderboard(TestServer server)
+        {
+            var client = server.CreateClient();
+            var response = await client.GetAsync("/leaderboard");
+            response.EnsureSuccessStatusCode();
+            var resultString = await response.Content.ReadAsStringAsync();
+            return JArray.Parse(resultString);
         }
     }
 
@@ -44,7 +74,7 @@ namespace Leaderboard.Test
     public class HttpFootballEventsSource_should
     {
         [Test, Timeout(6000)]
-        //[Ignore("Focus integration test")]
+        [Ignore("Focus integration test")]
         public async Task Receive_valid_events_from_real_http_service()
         {
             var serviceProvider = new ServiceCollection().AddHttpClient().BuildServiceProvider();
@@ -52,12 +82,49 @@ namespace Leaderboard.Test
             var source = new HttpFootballEventsSource(httpClientFactory);
 
             var footballEvents = await source.FetchEvents();
-            foreach(var footballEvent in footballEvents)
+
+            CheckFootballEvents(footballEvents);
+        }
+
+        [Test]
+        public async Task Receive_valid_events_from_fake()
+        {
+            var source = new FakeFootballEventSource(new FootballEvent[] {
+                new FootballEvent { type = "game-start", gameId = "lyon-marseille" },
+                new FootballEvent { type = "game-start", gameId = "paris-monaco" },
+                new FootballEvent { type = "game-end", gameId = "paris-monaco" },
+                new FootballEvent { type = "goal", gameId = "lyon-marseille", team = "lyon" }
+            });
+
+            var footballEvents = await source.FetchEvents();
+
+            CheckFootballEvents(footballEvents);
+        }
+
+        private static void CheckFootballEvents(FootballEvent[] footballEvents)
+        {
+            foreach (var footballEvent in footballEvents)
             {
                 Check.That(footballEvent.gameId.Split('-')).CountIs(2);
                 Check.That(footballEvent.type).IsOneOfThese("game-start", "game-end", "goal");
             }
-            Check.That(footballEvents.Where(e => e.type == "goal")).ContainsOnlyElementsThatMatch(e => e.gameId.Contains(e.team));            
+            Check.That(footballEvents.Where(e => e.type == "goal")).ContainsOnlyElementsThatMatch(e => e.gameId.Contains(e.team));
+        }
+    }
+
+    internal class FakeFootballEventSource : IFootballEventsSource
+    {
+
+        FootballEvent[] _events;
+
+        public FakeFootballEventSource(FootballEvent[] events)
+        {
+            _events = events;
+        }
+
+        public Task<FootballEvent[]> FetchEvents()
+        {
+            return Task.FromResult(_events);
         }
     }
 }
